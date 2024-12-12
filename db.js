aec593d45dba0536c6dcf88bd2926c54a511c9ae
DidharSingh <160118600+DidharSingh@users.noreply.github.com>
2024-12-12 12:46:01 -0800

    db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/UserDB-6448', {
+    await mongoose.connect('mongodb+srv://didharsingh1321:Abc1234@cluster0.04zb6.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
